ad0439fa501546f4592296ab016574aa30bf691d
Brian Silah <unpervertedkid@gmail.com>
2023-03-19 13:40:03 +0300

    Project creation page

---
+++ src/main/java/com/silah/projecthub/views/projecthub/ProjectCreationView.java
@@ -0,0 +1,20 @@
+package com.silah.projecthub.views.projecthub;
+
+import com.silah.projecthub.views.projecthub.pagesections.Header;
+import com.silah.projecthub.views.projecthub.pagesections.ProjectCreationForm;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import java.util.List;
+import java.util.Optional;
+
+@PageTitle("Project Creation")
+@Route(value = "projects/create")
+public class ProjectCreationView extends VerticalLayout {
+    public ProjectCreationView() {
+        add(Header.createHeader(Optional.of(List.of(new H2("Create a new project")))));
+        add(new ProjectCreationForm());
+    }
+}
---
+++ src/main/java/com/silah/projecthub/views/projecthub/pagesections/ProjectCreationForm.java
@@ -0,0 +1,61 @@
+package com.silah.projecthub.views.projecthub.pagesections;
+
+import com.silah.projecthub.entities.ProjectCategory;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.richtexteditor.RichTextEditor;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class ProjectCreationForm extends VerticalLayout {
+    public ProjectCreationForm() {
+        add(createProjectCreationForm());
+    }
+
+    private HorizontalLayout createProjectCreationForm() {
+        TextField projectName = new TextField("Project Name");
+        projectName.setRequired(true);
+        projectName.setRequiredIndicatorVisible(true);
+
+        TextField projectDescription = new TextField("Project Description");
+        projectDescription.setRequired(true);
+        projectDescription.setRequiredIndicatorVisible(true);
+
+        Select<ProjectCategory> projectCategory = new Select<>();
+        projectCategory.setLabel("Project Category");
+        projectCategory.setItems(ProjectCategory.values());
+        projectCategory.setRequiredIndicatorVisible(true);
+
+
+
+        Text description = new Text("Project Guidelines");
+
+        RichTextEditor richTextEditor = new RichTextEditor();
+        richTextEditor.setMaxHeight("500px");
+        richTextEditor.setMaxWidth("500");
+
+        richTextEditor.setRequiredIndicatorVisible(true);
+
+
+        Button saveButton = new Button("Save");
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        saveButton.setMaxWidth("10px");
+
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(projectName, projectCategory, projectDescription, description, richTextEditor, saveButton);
+        formLayout.setColspan(projectDescription,3);
+        formLayout.setColspan(richTextEditor,3);
+
+        formLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("0", 1), new FormLayout.ResponsiveStep("21em", 2));
+
+        return new HorizontalLayout(formLayout);
+
+    }
+
+
+}
