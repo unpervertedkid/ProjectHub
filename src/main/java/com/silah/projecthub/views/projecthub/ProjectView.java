f201f94212edd22d1aa5c46138f5b95489105c5d
Brian Silah <unpervertedkid@gmail.com>
2023-03-24 13:55:04 +0300

    Displaying a save Project

--- src/main/java/com/silah/projecthub/views/projecthub/ProjectView.java
+++ src/main/java/com/silah/projecthub/views/projecthub/ProjectView.java
@@ -5,7 +5,12 @@ import com.silah.projecthub.services.ProjectService;
 import com.silah.projecthub.views.projecthub.pagesections.Header;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.richtexteditor.RichTextEditor;
+import com.vaadin.flow.component.richtexteditor.RichTextEditorVariant;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
@@ -27,12 +32,8 @@ public class ProjectView extends VerticalLayout implements HasUrlParameter<Long>
     public void setParameter(BeforeEvent beforeEvent, Long projectId) {
         Optional<Project> project = projectService.getProjectById(projectId);
         if (project.isPresent()){
-            add(Header.createHeader(Optional.of(List.of(new H1(project.get().getName())))));
-            Text text = new Text(project.get().getDescription());
-            add(text);
-
-            Text guide = new Text(project.get().getGuide());
-            add(guide);
+            loadProject(project);
+            loadFeedbackButtons();
 
         }
         else {
@@ -40,9 +41,46 @@ public class ProjectView extends VerticalLayout implements HasUrlParameter<Long>
         }
     }
 
+    private void loadFeedbackButtons() {
+        Icon likeIcon = new Icon("lumo", "thumbs-up");
+        Icon dislikeIcon = new Icon("lumo", "thumbs-down");
+
+        likeIcon.getStyle().set("margin-right", "5px");
+        dislikeIcon.getStyle().set("margin-right", "5px");
+
+        //Make the icons clickable
+        likeIcon.addClickListener(e -> {
+            Notification notification = new Notification("You liked this project", 3000);
+            notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+        });
+        dislikeIcon.addClickListener(e -> {
+            Notification notification = new Notification("You disliked this project", 3000);
+            notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+        });
+
+        add(likeIcon, dislikeIcon);
+    }
+
+    private void loadProject(Optional<Project> project) {
+        add(Header.createHeader(Optional.of(List.of(new H1(project.get().getName())))));
+        H1  descriptionTitle =new H1("Description");
+        Text description = new Text(project.get().getDescription());
+        add(descriptionTitle, description);
+
+        H1 guideHeader = new H1("Guide");
+        RichTextEditor guide = new RichTextEditor();
+        guide.setValue(project.get().getGuide());
+        guide.setReadOnly(true);
+        guide.addThemeVariants(RichTextEditorVariant.LUMO_COMPACT);
+        add(guideHeader,guide);
+    }
+
     private void display404() {
-        add(Header.createHeader(Optional.of(List.of(new H1("404")))));
-        H1 h1 = new H1("404");
+        add(Header.createHeader(Optional.of(List.of(new H1("Ooops!")))));
+        // TODO: Add a 404 page
+        H1 h1 = new H1("404: Page not found");
+        h1.getStyle().set("text-align", "center");
+        h1.getStyle().set("margin-top", "auto");
         h1.getStyle().set("margin-left", "auto");
         h1.getStyle().set("margin-right", "auto");
         h1.getStyle().set("display", "block");
