28d59bc3901d1d20a90d8d1838e5d5e983b08d7f
Brian Silah <unpervertedkid@gmail.com>
2023-03-25 18:35:10 +0300

    Code refactoring and organization

--- src/main/java/com/silah/projecthub/views/projecthub/PageNotFoundView.java
+++ src/main/java/com/silah/projecthub/views/projecthub/PageNotFoundView.java
@@ -17,24 +17,43 @@ public class PageNotFoundView extends VerticalLayout {
 
     private void displayPageNotFound() {
 
+        VerticalLayout PageNotFound = getLayout();
+
+        Image img = getImage();
+
+        H2 header = getHeader();
+
+        Paragraph description = getDescription();
+
+        PageNotFound.add(img, header, description);
+
+        add(PageNotFound);
+    }
+
+    private static VerticalLayout getLayout() {
         VerticalLayout PageNotFound = new VerticalLayout();
-        PageNotFound.setSpacing(false);
-
-        Image img = new Image("images/PageNotFound.png", "404 image ");
-        img.setWidth("200px");
-
-        H2 header = new H2("Yeah we know its a bummer, but...");
-        header.addClassNames(LumoUtility.Margin.Top.XLARGE, LumoUtility.Margin.Bottom.MEDIUM);
-
-        Paragraph paragraph = new Paragraph("The page you are looking for does not exist😔");
-
-        PageNotFound.add(img, header, paragraph);
-
         PageNotFound.setSizeFull();
         PageNotFound.setJustifyContentMode(JustifyContentMode.CENTER);
         PageNotFound.setAlignItems(Alignment.CENTER);
         PageNotFound.getStyle().set("text-align", "center");
+        PageNotFound.setSpacing(false);
+        return PageNotFound;
+    }
 
-        add(PageNotFound);
+    private static Paragraph getDescription() {
+        Paragraph description = new Paragraph("The page you are looking for does not exist😔.");
+        return description;
+    }
+
+    private static H2 getHeader() {
+        H2 header = new H2("Yeah we know its a bummer, but...");
+        header.addClassNames(LumoUtility.Margin.Top.XLARGE, LumoUtility.Margin.Bottom.MEDIUM);
+        return header;
+    }
+
+    private static Image getImage() {
+        Image img = new Image("images/PageNotFound.png", "404 image ");
+        img.setWidth("200px");
+        return img;
     }
 }
