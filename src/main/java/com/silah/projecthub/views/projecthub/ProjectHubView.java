351408472dcb726b3f0747b184f905587118b70e
Brian Silah <unpervertedkid@gmail.com>
2023-03-30 12:20:16 +0300

    Displaying all projects in the main/ home page

--- src/main/java/com/silah/projecthub/views/projecthub/ProjectHubView.java
+++ src/main/java/com/silah/projecthub/views/projecthub/ProjectHubView.java
@@ -1,64 +1,34 @@
 package com.silah.projecthub.views.projecthub;
 
 import com.silah.projecthub.entities.Project;
-import com.silah.projecthub.entities.ProjectCategory;
 import com.silah.projecthub.services.ProjectService;
 import com.silah.projecthub.views.projecthub.components.Card;
 import com.silah.projecthub.views.projecthub.pagesections.NavigationBar;
+import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
+import java.util.List;
+
 @PageTitle("ProjectHub")
 @Route(value = "projects")
 @RouteAlias(value = "")
-public class ProjectHubView extends HorizontalLayout {
-    private final ProjectService projectService;
+public class ProjectHubView extends AppLayout {
 
-    public ProjectHubView(ProjectService projectService) {
-        this.projectService = projectService;
-        add(new NavigationBar());
+    public ProjectHubView(ProjectService projectService, Card card) {
+        addToNavbar(new NavigationBar());
 
         getElement().getStyle().set("height", "100%");
 
+        List<Project> projects = projectService.getAllProjects();
 
-        Project testProject = new Project("Project 1", "Description 1", ProjectCategory.BACKEND, "A simple guide to doing this");
+        FlexLayout flexLayout = card.getProjectLayout(projects);
 
-
-        FlexLayout flexLayout = getProjectLayout(testProject);
-
-        add(flexLayout);
+        setContent(flexLayout);
 
 
-    }
-
-    private static FlexLayout getProjectLayout(Project testProject) {
-        FlexLayout flexLayout = new FlexLayout();
-        flexLayout.setWidthFull();
-        flexLayout.getStyle().set("padding", "10px");
-        flexLayout.getStyle().set("gap", "10px");
-        flexLayout.setFlexWrap(FlexLayout.FlexWrap.WRAP);
-        flexLayout.setAlignContent(FlexLayout.ContentAlignment.CENTER);
-
-        int maxCardsPerRow = 6;
-        int currentCardsInRow = 0;
-
-        for (int i = 0; i < 10; i++) {
-            FlexLayout customCard = Card.createCard(testProject);
-            flexLayout.add(customCard);
-
-            currentCardsInRow++;
-            if (currentCardsInRow == maxCardsPerRow) {
-                currentCardsInRow = 0;
-            }
-        }
-        return flexLayout;
-    }
-
-    private void loadAllProjects() {
-        // TODO: Load all projects from the database and display them
     }
 
 }
