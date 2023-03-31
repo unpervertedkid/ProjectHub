57c77462cf318155c55d23b921c8b29639916b44
Brian Silah <unpervertedkid@gmail.com>
2023-03-31 12:23:42 +0300

    Added menu bar logic

--- src/main/java/com/silah/projecthub/views/projecthub/components/Card.java
+++ src/main/java/com/silah/projecthub/views/projecthub/components/Card.java
@@ -4,10 +4,16 @@ import com.silah.projecthub.entities.Project;
 import com.silah.projecthub.entities.ProjectCategory;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -58,6 +64,9 @@ public class Card {
         int maxCardsPerRow = 6;
         int currentCardsInRow = 0;
 
+        Div menuBarDiv = getMenuBarDiv();
+        flexLayout.add(menuBarDiv);
+
         for (Project project : projects) {
             FlexLayout customCard = createCard(project);
             flexLayout.add(customCard);
@@ -68,6 +77,72 @@ public class Card {
             }
         }
         return flexLayout;
+    }
+
+    private Div getMenuBarDiv() {
+        Div menuBarDiv = new Div();
+        FlexLayout menuBarFlexLayout = new FlexLayout();
+        menuBarFlexLayout.setJustifyContentMode(FlexLayout.JustifyContentMode.BETWEEN);
+        menuBarFlexLayout.setWidthFull();
+        menuBarFlexLayout.getStyle().set("padding", "10px");
+        menuBarFlexLayout.getStyle().set("gap", "10px");
+        menuBarFlexLayout.getStyle().set("margin-bottom", "10px");
+
+        MenuBar menuBar = getMenuBar();
+        TextField searchField = getSearchField();
+        menuBarFlexLayout.add(menuBar, searchField);
+
+        menuBarDiv.add(menuBarFlexLayout);
+
+        menuBarDiv.setWidth("100%");
+
+        return menuBarDiv;
+    }
+
+    private TextField getSearchField() {
+        TextField searchField = new TextField();
+        searchField.setPlaceholder("Search");
+        searchField.setWidth("200px");
+        searchField.setPrefixComponent(VaadinIcon.SEARCH.create());
+        return searchField;
+    }
+
+    private MenuBar getMenuBar() {
+        MenuBar menuBar = new MenuBar();
+
+        menuBar.setOpenOnHover(true);
+
+        MenuItem categories = menuBar.addItem("Categories");
+        SubMenu categorySubMenu = categories.getSubMenu();
+        for(ProjectCategory category : ProjectCategory.values()){
+            categorySubMenu.addItem(category.toString());
+        }
+
+        MenuItem languages = menuBar.addItem("Languages");
+        SubMenu languageSubMenu = languages.getSubMenu();
+        languageSubMenu.addItem("Java");
+        languageSubMenu.addItem("Python");
+        languageSubMenu.addItem("C++");
+        languageSubMenu.addItem("C#");
+        languageSubMenu.addItem("JavaScript");
+        languageSubMenu.addItem("PHP");
+        languageSubMenu.addItem("Ruby");
+        languageSubMenu.addItem("Go");
+        languageSubMenu.addItem("Swift");
+
+
+        MenuItem sort = menuBar.addItem("Sort");
+        SubMenu sortSubMenu = sort.getSubMenu();
+        sortSubMenu.addItem("Newest");
+        sortSubMenu.addItem("Oldest");
+        sortSubMenu.addItem("Most Popular");
+        sortSubMenu.addItem("Least Popular");
+        sortSubMenu.addItem("Most Viewed");
+        sortSubMenu.addItem("Least Viewed");
+
+
+
+        return menuBar;
     }
 
 }
