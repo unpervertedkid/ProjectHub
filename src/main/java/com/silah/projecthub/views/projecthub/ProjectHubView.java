c6182c5a822d4cdd2b4c460e56a3d575104fb7dd
Brian Silah <unpervertedkid@gmail.com>
2023-03-31 12:23:23 +0300

    Removed menu bar logic from project view page and moved it to the card class. Probably going to move it back...

--- src/main/java/com/silah/projecthub/views/projecthub/ProjectHubView.java
+++ src/main/java/com/silah/projecthub/views/projecthub/ProjectHubView.java
@@ -5,6 +5,8 @@ import com.silah.projecthub.services.ProjectService;
 import com.silah.projecthub.views.projecthub.components.Card;
 import com.silah.projecthub.views.projecthub.pagesections.NavigationBar;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.menubar.MenuBar;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -16,6 +18,7 @@ import java.util.List;
 @Route(value = "projects")
 @RouteAlias(value = "")
 public class ProjectHubView extends AppLayout {
+    FlexLayout projectLayout;
 
     public ProjectHubView(ProjectService projectService, Card card) {
         addToNavbar(new NavigationBar());
@@ -24,11 +27,22 @@ public class ProjectHubView extends AppLayout {
 
         List<Project> projects = projectService.getAllProjects();
 
-        FlexLayout flexLayout = card.getProjectLayout(projects);
+        MenuBar menuBar = getMenuBar();
 
-        setContent(flexLayout);
+        projectLayout = card.getProjectLayout(projects);
 
-
+        setContent(projectLayout);
     }
+
+    private MenuBar getMenuBar() {
+        MenuBar menuBar = new MenuBar();
+
+        MenuItem categories = menuBar.addItem("Categories");
+        MenuItem languages = menuBar.addItem("Languages");
+        MenuItem sort = menuBar.addItem("Sort");
+
+        return menuBar;
+    }
+
 
 }
